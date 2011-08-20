b6a18271d75a4958b7fb01f06ea81b003683b8a3
artemave <artemave@gmail.com>
2011-08-20 23:47:53 +0100

    Compensate for mouse 'noise' movements; some refactoring around figuring out which event actually triggers translate (depending on options)

--- contentscript.js
+++ contentscript.js
@@ -1,20 +1,6 @@
 $.noConflict();
 (function($) {
   chrome.extension.sendRequest({handler: 'get_options'}, function(response) {
-    var options = JSON.parse( response.options );
-
-    var $debug = false;
-
-    var original_console_log = console.log;
-    console.log = function(arg) {
-      if ($debug) {
-        original_console_log.call(this, arg);
-      }
-    }
-
-    var tooltip = new Tooltip();
-    var start_tip = new Tooltip();
-
     function process(e) {
 
       //TODO option to show translation in a growl type popup (in the corner)
@@ -116,109 +102,145 @@ $.noConflict();
         return hit_word;
       }
 
-      if (!options.target_lang) {
-        if (start_tip.is(':hidden')) {
-          start_tip.show(e.clientX, e.clientY, 'Please, <a href="'+chrome.extension.getURL('options.html')+'">choose</a> language to translate into.');
+      function show_result(response) {
+        function deserialize(text) {
+          var res;
+
+          try {
+            res = JSON.parse(text);
+          }
+          catch (e) {
+            // that means text is string as opposed to serialized object
+            if (e.toString() == 'SyntaxError: Unexpected token ILLEGAL') {
+              res = text;
+            }
+            else {
+              throw e;
+            }
+          }
+          return res;
+        };
+
+        console.log('response: "'+response.source_lang+'" '+response.translation);
+
+        if (options.target_lang == response.source_lang) {
+          console.log('skipping translation into the same language');
+          return;
+        }
+
+        var translation = deserialize(response.translation);
+        var formatted_translation = '';
+
+        if (translation instanceof Array) {
+          _.each(translation, function(pos_block) {
+              var formatted_pos = pos_block.pos ? '<bolds>'+pos_block.pos+'</bolds>: ' : '';
+              var formatted_meanings = pos_block.meanings.slice(0,5).join(', ') + ( pos_block.meanings.length > 5 ? '...' : '' );
+              formatted_translation = formatted_translation + formatted_pos + formatted_meanings + '<br/>';
+          });
+        }
+        else {
+          formatted_translation = translation;
+        }
+
+        tooltip.show(e.clientX, e.clientY, formatted_translation);
+      };
+
+      var selection = window.getSelection();
+      var hit_elem = document.elementFromPoint(e.clientX, e.clientY);
+
+      //don't mess with html inputs
+      if (/INPUT|TEXTAREA/.test( hit_elem.nodeName )) {
+        return;
+      }
+
+      var word = '';
+      if (selection.toString() != '') {
+        console.log('Got selection: ' + selection.toString());
+
+        var sel_container = selection.getRangeAt(0).commonAncestorContainer;
+
+        while (sel_container.nodeType != Node.ELEMENT_NODE) {
+          sel_container = sel_container.parentNode;
+        }
+
+        if (
+          // only choose selection if mouse stopped within immediate parent of selection
+          ( $(hit_elem).is(sel_container) || $.contains(sel_container, hit_elem) )
+          // and since it can still be quite a large area
+          // narrow it down by only choosing selection if mouse points at the element that is (partially) inside selection
+          && selection.containsNode(hit_elem, true)
+            // But what is the point for the first part of condition? Well, without it, pointing at body for instance would also satisfy the second part
+            // resulting in selection translation showing up in random places
+        ) {
+          word = selection.toString();
+        }
+        else if (options.translate_by == 'point') {
+          word = getHitWord(e);
         }
       }
       else {
+        word = getHitWord(e);
+      }
+      if (word != '') {
+        chrome.extension.sendRequest({handler: 'translate', word: word}, show_result);
+      }
+    }
+
+    function hasMouseReallyMoved(e) { //or is it a tremor?
+      var left_boundry = parseInt(last_mouse_stop.x) - 5,
+        right_boundry  = parseInt(last_mouse_stop.x) + 5,
+        top_boundry    = parseInt(last_mouse_stop.y) - 5,
+        bottom_boundry = parseInt(last_mouse_stop.y) + 5;
+
+      return e.clientX > right_boundry || e.clientX < left_boundry || e.clientY > bottom_boundry || e.clientY < top_boundry;
+    }
+
+    function withOptionsSatisfied(e, do_stuff) {
+      if (options.target_lang) {
         //respect 'translate only when shift pressed' option
         if (options.shift_only && !shift_pressed) { return }
-
-        //respect 'translate by click' option
-        //but fallback to mousestop to translate selections
-        if (window.getSelection().toString() == '' && options.translate_by == 'click' && e.type != 'click') { return }
 
         //respect "don't translate these sites"
         if ($.grep(options.except_urls, function(url) { return RegExp(url).test(window.location.href) }).length > 0) { return }
 
-        function show_result(response) {
-          function deserialize(text) {
-            var res;
-
-            try {
-              res = JSON.parse(text);
-            }
-            catch (e) {
-              // that means text is string as opposed to serialized object
-              if (e.toString() == 'SyntaxError: Unexpected token ILLEGAL') {
-                res = text;
-              }
-              else {
-                throw e;
-              }
-            }
-            return res;
-          };
-
-          console.log('response: "'+response.source_lang+'" '+response.translation);
-
-          if (options.target_lang == response.source_lang) {
-            console.log('skipping translation into the same language');
-            return;
-          }
-
-          var translation = deserialize(response.translation);
-          var formatted_translation = '';
-
-          if (translation instanceof Array) {
-            _.each(translation, function(pos_block) {
-                var formatted_pos = pos_block.pos ? '<bolds>'+pos_block.pos+'</bolds>: ' : '';
-                var formatted_meanings = pos_block.meanings.slice(0,5).join(', ') + ( pos_block.meanings.length > 5 ? '...' : '' );
-                formatted_translation = formatted_translation + formatted_pos + formatted_meanings + '<br/>';
-            });
-          }
-          else {
-            formatted_translation = translation;
-          }
-
-          tooltip.show(e.clientX, e.clientY, formatted_translation);
-        };
-
-        var selection = window.getSelection();
-        var hit_elem = document.elementFromPoint(e.clientX, e.clientY);
-
-        //don't mess with html inputs
-        if (/INPUT|TEXTAREA/.test( hit_elem.nodeName )) {
-          return;
-        }
-
-        var word = '';
-        if (selection.toString() != '') {
-          console.log('Got selection: ' + selection.toString());
-
-          var sel_container = selection.getRangeAt(0).commonAncestorContainer;
-
-          while (sel_container.nodeType != Node.ELEMENT_NODE) {
-            sel_container = sel_container.parentNode;
-          }
-
-          if (
-            // only choose selection if mouse stopped within immediate parent of selection
-            ( $(hit_elem).is(sel_container) || $.contains(sel_container, hit_elem) )
-            // and since it can still be quite a large area
-            // narrow it down by only choosing selection if mouse points at the element that is (partially) inside selection
-            && selection.containsNode(hit_elem, true)
-              // But what is the point for the first part of condition? Well, without it, pointing at body for instance would also satisfy the second part
-              // resulting in selection translation showing up in random places
-          ) {
-            word = selection.toString();
-          }
-          else if (options.translate_by == 'point') {
-            word = getHitWord(e);
-          }
-        }
-        else {
-          word = getHitWord(e);
-        }
-        if (word != '') {
-          chrome.extension.sendRequest({handler: 'translate', word: word}, show_result);
+        do_stuff();
+      }
+      else {
+        if (start_tip.is(':hidden')) {
+          start_tip.show(e.clientX, e.clientY, 'Please, <a href="'+chrome.extension.getURL('options.html')+'">choose language</a> to translate into.');
         }
       }
     }
 
-    $(document).bind('mousestop', process);
-    $(document).click(process);
+    var options = JSON.parse( response.options );
+
+    var $debug = false;
+
+    var original_console_log = console.log;
+    console.log = function(arg) {
+      if ($debug) {
+        original_console_log.call(this, arg);
+      }
+    }
+
+    var tooltip = new Tooltip();
+    var start_tip = new Tooltip();
+
+    $(document).bind('mousestop', function(e) {
+        withOptionsSatisfied(e, function() {
+            //translate selection on hover regardless
+            if (options.translate_by == 'point' || window.getSelection().toString() != '') {
+              process(e);
+            }
+        });
+    });
+    $(document).click(function(e) {
+        withOptionsSatisfied(e, function() {
+            if (options.translate_by == 'click') {
+              process(e);
+            }
+        });
+    });
 
     var shift_pressed = false;
     $(document)
@@ -233,31 +255,28 @@ $.noConflict();
       });
 
     var timer25;
+    var last_mouse_stop = {x: 0, y: 0};
+
     // setup mousestop event
-    // mousestop triggers the entire thing - it is an entry point
     $(document).mousemove(function(e){
       clearTimeout(timer25);
 
       timer25 = setTimeout(function() {
-
-        if (last_x != e.clientX || last_y != e.clientY) { return }
+        if (!hasMouseReallyMoved(e)) { return }
 
         var mousestop = new $.Event("mousestop");
-        mousestop.clientX = e.clientX;
-        mousestop.clientY = e.clientY;
+        last_mouse_stop.x = mousestop.clientX = e.clientX;
+        last_mouse_stop.y = mousestop.clientY = e.clientY;
+
         $(document).trigger(mousestop);
       }, options.shift_only ? 200 : options.delay);
     });
 
-    var last_x, last_y;
-
     // hide translation on any move
     $(document).mousemove(function(e) {
-      if (last_x != e.clientX || last_y != e.clientY) {
+      if (hasMouseReallyMoved(e)) {
         tooltip.hide();
       }
-      last_x = e.clientX;
-      last_y = e.clientY;
     });
     $(window).scroll(function() { tooltip.hide() });
 
